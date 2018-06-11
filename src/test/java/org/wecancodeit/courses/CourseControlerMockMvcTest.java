885c317848a3393ea72d017e3fc105e507a2c370
JulieStuneck <jbstuneck@yahoo.com>
2018-06-10 20:11:09 -0400

    first commit - no Thymeleaf error message

---
+++ src/test/java/org/wecancodeit/courses/CourseControlerMockMvcTest.java
@@ -0,0 +1,23 @@
+package org.wecancodeit.courses;
+
+import javax.annotation.Resource;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(CourseController.class)
+public class CourseControlerMockMvcTest {
+
+	@Resource
+	private MockMvc mvc;
+	
+	@Test
+	public void shouldBeOKForAllCourses() throws Exception {
+		mvc.perform(get("/show-course")).andExpect(status().isOk());
+}}
